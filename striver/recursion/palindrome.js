2e32df29f54f848770aa695075df593f3969db3e
Bikram Jethi <bikramjethi@gmail.com>
2025-09-08 08:09:49 +0530

    recursion: basics

---
+++ striver/recursion/palindrome.js
@@ -0,0 +1,21 @@
+/**
+ * Recursion:
+ * - Function calls itself
+ * - Base condition to exit
+ * 
+ * Given a string s, return true if the string is palindrome, otherwise false
+   A string is called palindrome if it reads the same forward and backward.
+
+   Examples:
+   Input : s = "hannah"
+   Output : true
+   Explanation : The string when reversed is --> "hannah", which is same as original string , so we return true.
+
+   Input : s = "aabbaaa"
+   Output : false
+   Explanation : The string when reversed is --> "aaabbaa", which is not same as original string, So we return false.
+ * 
+ */
+
+
+
---
+++ striver/recursion/reverse-an-array.js
@@ -0,0 +1,40 @@
+/**
+ * Reverse an array 
+   Given an array arr of n elements. The task is to reverse the given array. 
+   The reversal of array should be inplace.
+
+   Examples:
+   Input: n=5, arr = [1,2,3,4,5]
+   Output: [5,4,3,2,1]
+   Explanation: The reverse of the array [1,2,3,4,5] is [5,4,3,2,1]
+   
+   Input: n=6, arr = [1,2,1,1,5,1]
+   Output: [1,5,1,1,2,1]
+   Explanation: The reverse of the array [1,2,1,1,5,1] is [1,5,1,1,2,1].
+ * 
+ */
+
+
+
+/**
+ * Reverse an array - Using 2 pointers
+ */
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        const temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+
+    return s;
+};
